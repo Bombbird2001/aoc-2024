67d56930578b01fcd8ad35cdaacca71593b82138
Shang Yi Ku <kushangyi2001@gmail.com>
2024-12-14 18:33:37 +0800

    Finish day 5 part 2, day 6

--- d5/src/main.rs
+++ d5/src/main.rs
@@ -10,6 +10,7 @@ fn main() -> Result<()> {
     let mut before_set = HashSet::new();
     let mut all_pairs_added = false;
     let mut correct_sum = 0;
+    let mut incorrect_lines = Vec::new();
     contents.lines().for_each(|line| {
         if line.is_empty() {
             all_pairs_added = true;
@@ -25,6 +26,7 @@ fn main() -> Result<()> {
                     let next_no = nos[j];
                     let combined = next_no.to_owned() + "|" + curr_no;
                     if before_set.contains(combined.as_str()) {
+                        incorrect_lines.push(line);
                         return;
                     }
                 }
@@ -35,6 +37,27 @@ fn main() -> Result<()> {
     });
 
     println!("Correct sum: {}", correct_sum);
+
+    let mut corrected_sum = 0;
+    incorrect_lines.iter().for_each(|line| {
+        // Count number of elements, then add only the element with exactly floor(n/2) entries before/after it
+        let nos = line.split(",").collect::<Vec<_>>();
+        let length = nos.len();
+        nos.iter().for_each(|no| {
+            let mut count: usize = 0;
+            nos.iter().for_each(|n| {
+                let combined = (*n).to_owned() + "|" + no;
+                if before_set.contains(combined.as_str()) {
+                    count += 1;
+                }
+            });
+            if count == length / 2 {
+                corrected_sum += no.parse::<i32>().unwrap();
+            }
+        });
+    });
+
+    println!("Corrected sum: {}", corrected_sum);
 
     Ok(())
 }
